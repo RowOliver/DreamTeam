598125879c147a21025dac12beda13bfc3a70728
Rowan Oliver <r.oliver.yoga@gmail.com>
2020-12-10 11:47:39 -0800

    possible framework

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,25 @@
+// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name= name;
+        this.email = email;
+        this.id = id;
+    
+    }
+
+    getName(){
+        return this.name;
+
+    }
+
+    getId(){
+        return this.id;
+
+    }
+
+    getEmail(){
+        return this.email;
+    }
+}
+
+module.exports = Employee;
---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./Develop/lib/Engineer.js");
+const Intern = require("./Develop/lib/Intern.js");
+const Manager = require("./Develop/lib/Manager.js");
+
